4701bec6b0c8c4038fded826190ed2d677bc8e94
YNNJN <dbswls1014@naver.com>
2020-04-19 17:35:31 +0900

    Update solving process

--- 나누어 떨어지는 숫자 배열.py
+++ 나누어 떨어지는 숫자 배열.py
@@ -1,10 +1,16 @@
-def solution(arr, divisor):
-    answer = []
-    for i in arr:
-        if i % divisor == 0:
-            answer.append(i)
-    answer.sort()
-    if len(answer) == 0:
-        answer = [-1]
+# def solution(arr, divisor):
+#     answer = []
+#     for i in arr:
+#         if i % divisor == 0:
+#             answer.append(i)
+#     answer.sort()
+#     if len(answer) == 0:
+#         answer = [-1]
+#
+#     return answer
 
-    return answer
+#list comprehension
+def solution(arr, divisor):
+    array = [x for x in arr if x % divisor == 0]
+    array.sort()
+    return array if len(array) else [-1]
