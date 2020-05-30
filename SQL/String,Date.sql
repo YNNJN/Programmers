d8e15506f7bde57a7ea5ba39e3da4577774a5559
김윤진 <myung2ing@gmail.com>
2020-05-30 22:13:57 +0900

    sql probs

---
+++ SQL/String,Date.sql
@@ -0,0 +1,20 @@
+-- 루시와 엘라 찾기
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME IN ('Lucy','Ella','Pickle','Rogan','Sabrina','Mitty')
+ORDER BY ANIMAL_ID
+
+-- 이름에 el이 들어가는 동물 찾기
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE ANIMAL_TYPE = 'Dog' AND (NAME LIKE '%el%' OR NAME LIKE '%El%')
+ORDER BY NAME
+
+-- 중성화 여부 파악하기
+SELECT ANIMAL_ID, NAME,
+    (CASE  WHEN SEX_UPON_INTAKE LIKE 'Neutered%' THEN 'O'
+           WHEN SEX_UPON_INTAKE LIKE 'Spayed%' THEN 'O'
+            ELSE 'X'
+       END) AS "중성화"
+    FROM ANIMAL_INS
+    ORDER BY ANIMAL_ID
